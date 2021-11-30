babadebfb1e4e2080629ce07494d42b6afae1df3
Anti-Shulk <justinschwerdtfeger245@gmail.com>
2021-11-30 12:43:24 -0600

    Fixed problems with auto vanishing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/paths/BlueCarosel.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.auto.paths;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.hardware.Hardware13266;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.Drive13266;
+import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
+
+@Autonomous(name="Blue Carosel Road Runner Path", group="Road Runner Paths")
+public class BlueCarosel extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        Drive13266 drive = new Drive13266(hardwareMap);
+        Hardware13266 robot = new Hardware13266();
+        robot.init(hardwareMap);
+
+        Pose2d startPose = new Pose2d(-47, 60, Math.toRadians(270));
+        ElapsedTime timer = new ElapsedTime();
+
+        drive.setPoseEstimate(startPose);
+
+        TrajectorySequence Trajectory1 = drive.trajectorySequenceBuilder(startPose)
+                .forward(10)
+                .turn(Math.toRadians(30)) // heading = 300
+                .back(7)
+                .build();
+
+        TrajectorySequence Trajectory2 = drive.trajectorySequenceBuilder(Trajectory1.end())
+                .forward(6)
+                .turn(Math.toRadians(-210)) // heading = 90
+                .back(30)
+                .turn(Math.toRadians(90)) // heading = 180
+                .back(26)
+                .build();
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        drive.followTrajectorySequence((Trajectory1));
+        robot.carouselMotor.setPower(robot.CAROUSEL_POWER);
+        sleep(5000);
+        robot.carouselMotor.setPower(0);
+        drive.followTrajectorySequence((Trajectory2));
+        robot.armMotor.setPower(robot.ARM_POWER);
+        robot.setArmPosition(robot.ARM_HIGH_POS);
+        sleep(2000);
+        robot.boxServo.setPosition(robot.BOX_DROP);
+        sleep(2000);
+    }
+}
+
