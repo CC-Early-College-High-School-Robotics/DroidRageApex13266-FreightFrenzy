ed5b8eee04299b144ced1bb9aea76d47a20442a8
chris <christopher9ram@gmail.com>
2021-12-26 19:38:01 -0600

    turret

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystembase/main/TurretSubsystem.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.hardware.subsystembase.main;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.hardware.Devices;
+import org.firstinspires.ftc.teamcode.hardware.subsystembase.base.BaseSubsystem;
+
+public class TurretSubsystem extends BaseSubsystem {
+    // Values
+    public static double TURRET_SERVO_1_FRONT = 1;
+    public static double TURRET_SERVO_2_FRONT = 1;
+
+    public static double TURRET_SERVO_1_LEFT = .5;
+    public static double TURRET_SERVO_2_LEFT = .5;
+
+    public static double TURRET_SERVO_1_RIGHT = 0;
+    public static double TURRET_SERVO_2_RIGHT = 0;
+
+    // Create hardware variables
+    public Servo turretServo1 = null;
+    public Servo turretServo2 = null;
+    // Constructor
+    public TurretSubsystem() {
+
+    }
+
+    // Initialize hardware variables
+    public void init(HardwareMap hardwareMap, Telemetry telemetry) {
+        super.init(hardwareMap, telemetry);
+        turretServo1 = hardwareMap.get(Servo.class,"turretServo1");
+        turretServo2 = hardwareMap.get(Servo.class,"turretServo2");
+        turretServo1.setDirection(Servo.Direction.FORWARD);
+        turretServo1.setDirection(Servo.Direction.FORWARD);
+    }
+
+    // Default command
+    public void defaultCommand(Gamepad gamepad1, Gamepad gamepad2) {
+        super.gamepadInit(gamepad1, gamepad2);
+        // Reset
+        if (gamepad2.a) {
+            turretServo1.setPosition(TURRET_SERVO_1_FRONT);
+            turretServo2.setPosition(TURRET_SERVO_2_FRONT);
+        }
+        // right
+        if (gamepad2.x) {
+            turretServo1.setPosition(TURRET_SERVO_1_RIGHT);
+            turretServo2.setPosition(TURRET_SERVO_2_RIGHT);
+        }
+        // left
+        if (gamepad2.b) {
+            turretServo1.setPosition(TURRET_SERVO_1_LEFT);
+            turretServo2.setPosition(TURRET_SERVO_2_LEFT);
+        }
+        // intake/reset position
+        if (gamepad2.y) {
+            turretServo1.setPosition(TURRET_SERVO_1_FRONT);
+            turretServo2.setPosition(TURRET_SERVO_2_FRONT);
+        }
+    }
+}
