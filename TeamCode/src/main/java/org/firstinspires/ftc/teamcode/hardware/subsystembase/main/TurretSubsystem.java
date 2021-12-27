5bac6e6dab06f6dda6d8413402987dc667e2b7cb
justin <justinschwerdtfeger245@gmail.com>
2021-12-27 17:48:56 -0600

    turret in ftc dashbord

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystembase/main/TurretSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystembase/main/TurretSubsystem.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.hardware.subsystembase.main;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -7,6 +8,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.hardware.subsystembase.base.BaseSubsystem;
 
+@Config
 public class TurretSubsystem extends BaseSubsystem {
     // Values
     public static double TURRET_SERVOS_FRONT = .210;
