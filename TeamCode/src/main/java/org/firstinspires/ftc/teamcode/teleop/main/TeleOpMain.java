198162b136b424e081e33ee1d505c327491194c6
justin <justinschwerdtfeger245@gmail.com>
2021-12-26 22:49:31 -0600

    alliance marker remove

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/main/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/main/TeleOpMain.java
@@ -11,6 +11,7 @@ import org.firstinspires.ftc.teamcode.hardware.subsystembase.main.BoxSubsystem;
 import org.firstinspires.ftc.teamcode.hardware.subsystembase.main.BreakModeSubsystem;
 import org.firstinspires.ftc.teamcode.hardware.subsystembase.main.CarouselSubsystem;
 import org.firstinspires.ftc.teamcode.hardware.subsystembase.main.DrivetrainSubsystem;
+import org.firstinspires.ftc.teamcode.hardware.subsystembase.main.FlipperSubsystem;
 import org.firstinspires.ftc.teamcode.hardware.subsystembase.main.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.hardware.subsystembase.main.TelemetrySubsystem;
 import org.firstinspires.ftc.teamcode.hardware.subsystembase.main.TurretSubsystem;
@@ -19,7 +20,7 @@ import org.firstinspires.ftc.teamcode.teleop.testing.TuningStart;
 @TeleOp(name="TeleOP Main")
 public class TeleOpMain extends LinearOpMode {
     // Declare subsystems
-    AllianceMarkerStickSubsystem stick          = new AllianceMarkerStickSubsystem          ();
+//    AllianceMarkerStickSubsystem stick          = new AllianceMarkerStickSubsystem          ();
     ArmSubsystem arm                            = new ArmSubsystem                          ();
     BoxSubsystem box                            = new BoxSubsystem                          ();
     CarouselSubsystem carousel                  = new CarouselSubsystem                     ();
@@ -28,19 +29,21 @@ public class TeleOpMain extends LinearOpMode {
     BreakModeSubsystem breakMode                = new BreakModeSubsystem                    ();
     TelemetrySubsystem telemetrySubsystem       = new TelemetrySubsystem                    ();
     TurretSubsystem turret                      = new TurretSubsystem();
+    FlipperSubsystem flipper                    = new FlipperSubsystem();
 
     @Override
     public void runOpMode() {
         // Initialize subsystems
-        stick.init(hardwareMap, telemetry);
+//        stick.init(hardwareMap, telemetry);
         arm.init(hardwareMap, telemetry);
         box.init(hardwareMap, telemetry);
         carousel.init(hardwareMap, telemetry);
         drivetrain.init(hardwareMap, telemetry);
         intake.init(hardwareMap, telemetry);
         turret.init(hardwareMap, telemetry);
+        flipper.init(hardwareMap,telemetry);
 
-        telemetrySubsystem.init(telemetry, stick, arm, box, carousel, drivetrain, intake, breakMode);
+        telemetrySubsystem.init(telemetry, arm, box, carousel, drivetrain, intake, breakMode);
         breakMode.init(gamepad1, gamepad2, arm, carousel, drivetrain);
 
         TuningStart.initializeTuning();
@@ -53,7 +56,7 @@ public class TeleOpMain extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            stick.defaultCommand(gamepad1, gamepad2);
+//            stick.defaultCommand(gamepad1, gamepad2);
             arm.findArmPosition(gamepad1, gamepad2);
             arm.setArmPosition(gamepad1, gamepad2);
             box.defaultCommand(gamepad1, gamepad2);
