ed888ef9cdc9dcdbdc65893a87f50712966cb3c5
justin <justinschwerdtfeger245@gmail.com>
2021-12-27 18:52:41 -0600

    box has 2 intake positions sorta ig its a bad name for it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystembase/main/BoxSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystembase/main/BoxSubsystem.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.hardware.Devices;
 import org.firstinspires.ftc.teamcode.hardware.subsystembase.base.BaseSubsystem;
 
 @Config
@@ -15,16 +14,19 @@ public class BoxSubsystem extends BaseSubsystem {
     public static double BOX_UP = 0.892;
     public static double BOX_AUTO_APPROACH_HUB = 0.500;
     public static double BOX_INTAKE = 0.02;
+    public static double BOX_INTAKE_FORWARD = 0.02;
     public static double BOX_DROP = 0.180;
     public static double BOX_INTERMEDIATE = 0.3;
     public static double BOX_UP_WAIT = 0.2;
     public static double BOX_INTAKE_WAIT = 0.45;
+    public static double BOX_INTAKE_SECOND_WAIT = 0.2;
 
     // Create hardware variables
     public Servo boxServo = null;
     double targetTime = .35;
     boolean setUpPos = false;
     boolean setIntakePos = false;
+    boolean setIntakeSecondPos = false;
 
     // Constructor
     public BoxSubsystem() {
@@ -68,8 +70,14 @@ public class BoxSubsystem extends BaseSubsystem {
             targetTime = runtime.seconds() + BOX_INTAKE_WAIT;
         }
         if (setIntakePos && runtime.seconds() >= targetTime) {
-            boxServo.setPosition(BOX_INTAKE);
+            boxServo.setPosition(BOX_INTAKE_FORWARD);
             setIntakePos = false;
+            setIntakeSecondPos = true;
+            targetTime = runtime.seconds() + BOX_INTAKE_SECOND_WAIT;
+        }
+        if (setIntakeSecondPos && runtime.seconds() >= targetTime) {
+            boxServo.setPosition(BOX_INTAKE);
+            setIntakeSecondPos = false;
         }
 
 
